0a27fd00a0206bf53dfd353d9853da1a6fc3c036
luzhuang <364439895@qq.com>
2026-01-15 01:07:35 +0800

    refactor(cooking): 移除冗余的 createConvexMeshWithIndices API
    - 删除带 indices 参数的 createConvexMeshFromBuffer 重载
    - 删除 createConvexMeshWithIndices JS 绑定
    - 保留简洁的 createConvexMesh(vertices, vertCount, physics)
    
    原因：eCOMPUTE_CONVEX 模式下 PhysX 只使用顶点数据计算凸包，
    indices 参数被完全忽略，indexCount 参数未被使用。

--- physx/source/physxwebbindings/src/binding/CookingBinding.h
+++ physx/source/physxwebbindings/src/binding/CookingBinding.h
@@ -13,32 +13,6 @@
 using namespace physx;
 using namespace emscripten;
 
-PxConvexMesh *createConvexMeshFromBuffer(int vertices,
-                                         PxU32 vertCount,
-                                         int indices,
-                                         PxU32 indexCount,
-                                         bool isU16,
-                                         PxCooking &cooking,
-                                         PxPhysics &physics) {
-    PxConvexMeshDesc convexDesc;
-    convexDesc.points.count = vertCount;
-    convexDesc.points.stride = sizeof(PxVec3);
-    convexDesc.points.data = (PxVec3 *)vertices;
-    if (isU16) {
-        convexDesc.indices.stride = 3 * sizeof(PxU16);
-        convexDesc.indices.data = (PxU16 *)indices;
-        convexDesc.flags = PxConvexFlag::e16_BIT_INDICES;
-    } else {
-        convexDesc.indices.stride = 3 * sizeof(PxU32);
-        convexDesc.indices.data = (PxU32 *)indices;
-    }
-    convexDesc.flags |= PxConvexFlag::eCOMPUTE_CONVEX;
-
-    PxConvexMesh *convexMesh = cooking.createConvexMesh(convexDesc, physics.getPhysicsInsertionCallback());
-
-    return convexMesh;
-}
-
 PxConvexMesh *createConvexMeshFromBuffer(int vertices, PxU32 vertCount, PxCooking &cooking, PxPhysics &physics) {
     PxConvexMeshDesc convexDesc;
     convexDesc.points.count = vertCount;
@@ -134,13 +108,6 @@ EMSCRIPTEN_BINDINGS(physx_cooking) {
 
     class_<PxCooking>("PxCooking")
             .function("release", &PxCooking::release)
-            .function("createConvexMeshWithIndices",
-                      optional_override([](PxCooking &cooking, int vertices, PxU32 vertCount, int indices,
-                                           PxU32 indexCount, bool isU16, PxPhysics &physics) {
-                          return createConvexMeshFromBuffer(vertices, vertCount, indices, indexCount, isU16, cooking,
-                                                            physics);
-                      }),
-                      allow_raw_pointers())
             .function("createConvexMesh",
                       optional_override([](PxCooking &cooking, int vertices, PxU32 vertCount, PxPhysics &physics) {
                           return createConvexMeshFromBuffer(vertices, vertCount, cooking, physics);
