79764ebcc133fcbfec28497327257d23b5a03c22
GuoLei1990 <gl3336563@163.com>
2026-02-22 14:23:45 +0800

    feat: 更新 README.md 中的 emsdk 版本，修改 CMakeLists.txt 和 PhysXWebBindings.cmake 以支持 C++17，添加新的 getUUID 函数以获取 PxRigidActor 的 UUID

--- physx/source/physxwebbindings/src/BindingHelper.h
+++ physx/source/physxwebbindings/src/BindingHelper.h
@@ -13,3 +13,7 @@ inline uint32_t getUUID(physx::PxShape* shape) {
 inline uint32_t getUUID(const physx::PxShape* shape) {
     return *static_cast<uint32_t*>(shape->userData);
 }
+
+inline uint32_t getUUID(const physx::PxRigidActor* actor) {
+    return *static_cast<uint32_t*>(actor->userData);
+}
